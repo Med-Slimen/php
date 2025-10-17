767cd7428a6a051d16a134723948c35b713947cd
Med-Slimen <mohamedamineslimene01@gmail.com>
2025-10-17 11:41:23 +0100

    tp4

---
+++ Probleme_solving/tp4/ex2/Entete.php
@@ -0,0 +1,7 @@
+<?php
+$nom="Slimene";
+$prenom= "Med";
+echo" nom :".$nom."<br> prenom :".$prenom."<br>";
+include("MaPage.php");
+include_once("MaPage.php");
+?>
---
+++ Probleme_solving/tp4/ex2/MaPage.php
@@ -0,0 +1,5 @@
+<?php
+$nom="Slimene";
+$prenom= "Med";
+echo" nom :".$nom."<br> prenom :".$prenom."";
+?>
