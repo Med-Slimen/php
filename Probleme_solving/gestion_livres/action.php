478a071b62d4067a50448c79429ec74ed4c4f4ea
Med-Slimen <mohamedamineslimene01@gmail.com>
2025-11-13 00:00:55 +0100

    gestion_livres

--- Probleme_solving/gestion_livres/action.php
+++ Probleme_solving/gestion_livres/action.php
@@ -6,17 +6,24 @@ $lib=$_POST["lib"];
 $aut=$_POST["aut"];
 $maison=$_POST["maison"];
 $nbt=$_POST["nbt"];
+$res=false;
 if($sel==1){
-    $res=false;
-    $query=$conn->prepare("INSERT INTO livres values(?,?,?,?,?,?)");
-    $query->bindValue(1,$code);
-    $query->bindValue(2,$lib);
-    $query->bindValue(3,$aut);
-    $query->bindValue(4,$maison);
-    $query->bindValue(5,$nbt);
-    $query->bindValue(6,$res);
-    $query->execute();
-
+    $sel=$conn->prepare("SELECT * FROM livres where code=?");
+    $sel->bindValue(1,$code);
+    $sel->execute();
+    if($sel->rowCount()>0){
+        echo"livre deja existe";
+    }
+    else{
+        $query=$conn->prepare("INSERT INTO livres values(?,?,?,?,?,?)");
+        $query->bindValue(1,$code);
+        $query->bindValue(2,$lib);
+        $query->bindValue(3,$aut);
+        $query->bindValue(4,$maison);
+        $query->bindValue(5,$nbt);
+        $query->bindValue(6,$res);
+        $query->execute();
+    }
 }
 else if($sel==2){
     $query=$conn->prepare("UPDATE livres set code=?,libelle=?,auteur=?,maison_edit=?,nb_tomes=? where code=?");
